48fa480e3715579ce1c349c20a4bad8a5d316feb
Lipinhol <84319271+Lipinhol@users.noreply.github.com>
2022-10-28 00:56:52 -0300

    Remover assentos

--- Aula06/exer01.py
+++ Aula06/exer01.py
@@ -1,5 +1,5 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
 soma = n1 + n2
 
 print('A soma entre {} e {} é {}.'.format(n1, n2, soma))
